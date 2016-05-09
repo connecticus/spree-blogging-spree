c7dcd9dc202917994e7bb5452baf187d2f4244f1
Michael Ray <michael@human-nyc.com>
2016-05-09 14:59:11 -0400

    crop sizes

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,10 +1,4 @@
 class Spree::BlogEntryImage < Spree::Image
-  has_attached_file :attachment
-
-  self.attachment_definitions[:attachment][:styles] = {
-    thumb: '100x100>',
-    medium: '410x272#',
-    large: '1270x848#'
-  }
+  has_attached_file :attachment, styles: { thumb: '100x100>', medium: '410x272#', large: '1270x848#' }
 
 end
