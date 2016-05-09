dfd794c25b5fd07e60f2334e8b7c7e93a359c428
Michael Ray <michael@human-nyc.com>
2016-05-09 11:26:46 -0400

    setting crop sizes

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,5 +1,10 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment
+
+  self.attachment_definitions[:attachment][:styles] = {
+    thumb: '100x100>',
+    medium: '410x272#'
+    large: '1270x848#'
+  }
+
 end
-
-
