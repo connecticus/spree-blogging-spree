2c2fbaa23f31478097f7dfe81b9b128e33c04b2e
Michael Ray <michael@human-nyc.com>
2016-05-09 15:21:29 -0400

    new crop size

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,7 +1,7 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
     styles: {
-      thumb: '100x100>',
+      thumb: '150x100#',
       medium: '410x272#',
       large: '1270x848#'
     }
