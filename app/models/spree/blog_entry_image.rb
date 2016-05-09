0b5a053f3b470582fcb36263e3d48999ac5032eb
Michael Ray <michael@human-nyc.com>
2016-05-09 16:09:59 -0400

    adding missing comma

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,6 +1,6 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
-                    styles: { thumb: '150x100#', medium: '410x272#', large: '1270x848#' }
+                    styles: { thumb: '150x100#', medium: '410x272#', large: '1270x848#' },
                     default_style: :medium,
                     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
   validates_attachment :attachment,
