a961d77f10038f8f91080c7d9a06449500c9f0d9
Michael Ray <michael@human-nyc.com>
2016-05-09 15:51:38 -0400

    blog entry image updating crop styles

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,9 +1,12 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
-    styles: {
-      thumb: '150x100#',
-      medium: '410x272#',
-      large: '1270x848#'
-    }
+                    styles: { thumb: '150x100#', medium: '410x272#', large: '1270x848#' }
+                    default_style: :medium,
+                    convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+  validates_attachment :attachment,
+    :presence => true,
+    :content_type => { :content_type => %w(image/jpeg image/jpg image/png image/gif) }
+
+
 
 end
