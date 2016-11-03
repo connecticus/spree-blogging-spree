a52b05ba41e65bbba95513c867cedbf65ca008b5
Paul Wagner <paulw@foodchain.com>
2016-11-03 09:54:40 -0700

    cleanup

--- config/initializers/act_as_taggable_on.rb
+++
@@ -1,7 +0,0 @@
-# act_as_taggable_on.rb
-# ActsAsTaggableOn::Tag.class_eval do
-#   has_friendly_id :name,
-#                   :use_slug => true,
-#                   :approximate_ascii => true,
-#                   :reserved_words => ['show', 'edit', 'create', 'update', 'destroy']
-# end
